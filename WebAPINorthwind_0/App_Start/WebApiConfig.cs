06fde24ddfb2c753187950d8d470697c470a7bd9
cagri.yolyapar <cagri.yolyapar@gmail.com>
2023-03-27 14:17:37 +0300

    API projesi test edildi sorun yok

--- WebAPINorthwind_0/App_Start/WebApiConfig.cs
+++ WebAPINorthwind_0/App_Start/WebApiConfig.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace WebAPINorthwind_0
 {
@@ -9,6 +10,12 @@ namespace WebAPINorthwind_0
     {
         public static void Register(HttpConfiguration config)
         {
+            //Cross Origins Resource Server
+
+            EnableCorsAttribute cors = new EnableCorsAttribute("*","*","*");
+            config.EnableCors(cors);
+
+            
             // Web API configuration and services
 
             // Web API routes
--- WebAPINorthwind_0/Controllers/CategoryController.cs
+++ WebAPINorthwind_0/Controllers/CategoryController.cs
@@ -6,6 +6,7 @@ using System.Net.Http;
 using System.Web.Http;
 using WebAPINorthwind_0.DesignPatterns.SingletonPattern;
 using WebAPINorthwind_0.Models;
+using WebAPINorthwind_0.RequestModels;
 using WebAPINorthwind_0.ResponseModels;
 
 namespace WebAPINorthwind_0.Controllers
@@ -36,6 +37,60 @@ namespace WebAPINorthwind_0.Controllers
         public List<CategoryResponseModel> BringCategories()
         {
             return ListCategories();
+        }
+
+        [HttpGet]
+        public CategoryResponseModel GetCategory(int id)
+        {
+            return _db.Categories.Where(x => x.CategoryID == id).Select(x => new CategoryResponseModel
+            {
+                ID = x.CategoryID,
+                CategoryName = x.CategoryName,
+                Description = x.Description
+            }).FirstOrDefault();
+        }
+
+        [HttpPost]
+        public List<CategoryResponseModel> AddCategory(CategoryCreateRequestModel item)
+        {
+            Category c = new Category
+            {
+                CategoryName = item.CategoryName,
+                Description = item.Description
+            };
+            _db.Categories.Add(c);
+            _db.SaveChanges();
+            return ListCategories();    
+        }
+
+        [HttpPut]
+        public List<CategoryResponseModel> UpdateCategory(CategoryUpdateRequestModel item)
+        {
+            Category guncellenecek = _db.Categories.Find(item.ID);
+            guncellenecek.CategoryName = item.CategoryName;
+            guncellenecek.Description = item.Description;
+            _db.SaveChanges();
+            return ListCategories();
+        }
+
+
+        [HttpDelete]
+        public List<CategoryResponseModel> DeleteCategory(int id)
+        {
+            _db.Categories.Remove(_db.Categories.Find(id));
+            _db.SaveChanges();
+            return ListCategories();
+        }
+
+        [HttpGet]
+        public List<CategoryResponseModel> SearchCategory(string item)
+        {
+            return _db.Categories.Where(x => x.CategoryName.Contains(item)).Select(x => new CategoryResponseModel
+            {
+                CategoryName = x.CategoryName,
+                Description = x.Description,
+                ID = x.CategoryID
+            }).ToList();
         }
 
     }
